58f7ee58c27528fe3c111858be12a68899954daa
James Mead <james@floehopper.org>
2018-05-08 18:32:21 +0100

    Calculate resistance from colour codes

--- server.rb
+++ server.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'resistor'
 
 get '/' do
-  {}.to_json
+  colours = params.fetch(:colours, '').split(',')
+  resistor = Resistor.new(colours)
+  {
+    resistance_in_ohms: resistor.ohm,
+    tolerance_in_percent: resistor.tolerance
+  }.to_json
 end
